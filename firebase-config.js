d82ba18c20d00d15db653eb39490d33713a8831c
Divya <138712402+divyat-thv@users.noreply.github.com>
2026-05-22 18:39:08 +0530

    Update Firebase storage bucket and comments

--- firebase-config.js
+++ firebase-config.js
@@ -3,18 +3,26 @@
 // ================================================
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyDOfkMvckJDRjVT7RKbtdDseYO9Zi9e3IE",
+
   authDomain: "aayushman-bhava.firebaseapp.com",
+
   projectId: "aayushman-bhava",
-  storageBucket: "aayushman-bhava.firebasestorage.app",
+
+  storageBucket: "aayushman-bhava.appspot.com",
+
   messagingSenderId: "27366797357",
+
   appId: "1:27366797357:web:ddb3072435aea51951c8a4",
+
   measurementId: "G-DL85YG5NPG"
 };
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Global access
+// Global objects
 window._fbAuth = firebase.auth();
+
 window._fbDb = firebase.firestore();
